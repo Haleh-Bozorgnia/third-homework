2e907eb4afc3bb04aea548b7ec44c91d419811b7
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-04-30 23:24:38 -0700

    Easy Going answered

---
+++ answers.js
@@ -0,0 +1,10 @@
+// Part I: HTML & CSS
+// Prepare yourself for tomorrow's morning exercise! Watch the following videos on HTML and CSS. You will be working on practicing CSS in your Morning Exercise.
+
+// Rank you comfort with the following material on a scale of 0 - 5:
+// 5 - I got this! I am ready to code
+
+for (let i=1; i<=20; i++)
+{
+    console.log(i);
+}
