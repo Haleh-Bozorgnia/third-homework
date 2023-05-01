169e5f2083f35384b631c99be191d036498e6c27
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-04-30 23:38:41 -0700

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -9,8 +9,19 @@
 //     console.log(i);
 // }
 
-for (let i = 0; i <= 200; i++) {
-  if (i % 2 == 0) {
+// for (let i = 0; i <= 200; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
     console.log(i);
   }
 }
