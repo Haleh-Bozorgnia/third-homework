629300ee92e457a74f182c8a0eb1db8f89c4f2d0
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-05-02 11:19:22 -0700

    Kristyn and Thom have their outfits ready for class - array practice

--- answers.js
+++ answers.js
@@ -152,3 +152,52 @@ nums.sort();
 console.log(nums);
 let median = Math.floor(nums.length / 2);
 console.log(nums[median]);
+////////////////////////////////
+// Alien Attire
+// ////////////////////////////
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps",
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],
+  [
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs",
+  ],
+  [
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans",
+  ],
+];
+let indexShoe = kristynsCloset.indexOf("left shoe");
+let kristynsShoe = kristynsCloset[indexShoe];
+console.log(kristynsShoe);
+thomsCloset[2].push(kristynsShoe);
+console.log(thomsCloset);
+////////////////////////////////
+// Dress Us Up
+// ////////////////////////////
+console.log(
+  `Kristyn will wear " ${kristynsCloset[1]} with ${kristynsCloset[3]} and ${kristynsCloset[5]}`
+);
+console.log(
+  `Thom will wear ${thomsCloset[1][1]} with ${thomsCloset[0][1]} and ${thomsCloset[2][0]}`
+);
